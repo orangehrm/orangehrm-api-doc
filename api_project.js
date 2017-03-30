8b10cc50ec7144d336dc557180dd6577ffaaa39c
Samantha Jayasinghe <isamantha@gmail.com>
2017-03-30 22:31:03 +1300

    modify leave module api doc

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-03-28T08:51:00.259Z",
+    "time": "2017-03-30T09:30:05.162Z",
     "url": "http://apidocjs.com",
     "version": "0.17.4"
   }
