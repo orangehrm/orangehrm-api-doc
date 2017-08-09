deb62a658cb0d232125f21d9d681453338e675e4
Samantha Jayasinghe <samantha.jayasinghe@totaralearning.com>
2017-08-10 10:52:50 +1200

    update api-doc 4.0 changes

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-07-06T23:08:03.365Z",
+    "time": "2017-08-09T22:51:49.971Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
